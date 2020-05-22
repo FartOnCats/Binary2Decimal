7be5ec673efd260c753445f1646a14081c0e9e5d
FartOnCats <19505519+FartOnCats@users.noreply.github.com>
2020-05-22 15:37:07 -0400

    Update b2d.py
    added ability to list values to convert for mass conversion

--- b2d.py
+++ b2d.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python
 
 import sys
-
 def printusage():
     usage = ""
     usage += "./b2d.py [option] [string]\n\n"
     usage += "Options: \n"
     usage += "  -b ; convert binary to decimal\n"
     usage += "  -d ; convert decimal to binary\n"
+    usage += "  -l ; convert multiple values seperated by commas"
     usage += "Example: ./b2d -b 0010 -> returns 2\n"
     usage += "Example: ./b2d -d 24 -> returns 11000\n"
+    usage += "Example: ./b2d.py -b -l 11110000,00111011  -> returns [240, 59]\n"
     usage += "Note: binary assumes the bit to the left is the lowest\n"
     usage += "  decimal will return highest bit to the right\n"
 
+
     print usage
-    sys.exit()
 def binary2decimal(binaryString):
     binaryString = binaryString[::-1]
     decimal = 0
@@ -23,8 +24,7 @@ def binary2decimal(binaryString):
         if int(binaryString[i]) == 1:
             decimal += 2**i
         i+=1
-    print decimal
-    sys.exit()
+    return decimal
 def decimal2binary(decimalString):
     #This is going to be inefficient due to my small brain
     decimal = int(decimalString)
@@ -37,21 +37,39 @@ def decimal2binary(decimalString):
     while highestPower >= 0:
         if decimal >= 2**highestPower:
             binaryString += "1"
-            print (decimal,2**highestPower)
             decimal = decimal - 2**highestPower
-            print decimal
         else:
            binaryString += "0"
            
         
         highestPower -=1
 
-    print binaryString
-    sys.exit()
+    return binaryString
+def listModeLogic(stringData):
+    strings = stringData.split(',')
+def auto(data):
+    
+    if "-d" in sys.argv:
+        output = decimal2binary(data)
+    elif "-b" in sys.argv:
+        output = binary2decimal(data)
+    else:
+        printusage()
+        sys.exit()
+    return output
+
 if len(sys.argv) < 2 :
     printusage()
-if sys.argv[1] == "-d":
-    decimal2binary(sys.argv[2])
-if sys.argv[1] == "-b":
-    binary2decimal(sys.argv[2])
-printusage()
+    sys.exit()
+
+if "-l" in sys.argv:
+    data = sys.argv[-1].split(",")
+    output = []
+    for i in data:
+        output.append(auto(i))
+    print(output)
+else:
+    print auto(sys.argv[-1])
+
+
+
